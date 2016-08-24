cbe6007c6eda9ed77b61c9f111910f4c0262fce8
gokulkrishh <krishnangokul9@gmail.com>
2016-08-24 20:16:45 +0530

    Updated terminal.sh

--- osx/terminal.sh
+++ osx/terminal.sh
@@ -1,6 +1,6 @@
 
 # Updating to pro theme
-open "~/dotfiles/.bash_profile.terminal"
+open ".bash_profile.terminal"
 sleep 1 # Wait a bit to make sure the theme is loaded
 
 defaults write com.apple.terminal "Default Window Settings" -string "Pro"
