94a78ca247413765bcf5fda07a0a605048047097
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2022-05-11 18:16:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -101,6 +101,7 @@ if test ! $(which nvm); then
   ## Set npm global config
   npm config set init.author.name "Gokulakrishnan Kalaikovan" ## Replace it with your name
   npm config set init.author.email "krishnangokul9@gmail.com" ## Replace it with your email id
+	npm config set init.author.url "https://gokul.site" ## Replace it with your url
 else
   e_warning "NVM is already installed. Skipping.."
 fi
