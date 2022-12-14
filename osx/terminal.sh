2a5b0b5edd9e5032a2e1224cf52a83285c5d8b78
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2022-12-14 09:03:09 +0530

    Update terminal.sh

--- osx/terminal.sh
+++ osx/terminal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Updating to custom dracula terminal theme
-open "Dracula.terminal"
+# Updating to zsh terminal theme
+open "bullet-train.terminal"
 
 sleep 1 # Wait a bit to make sure the theme is loaded
 
-defaults write com.apple.terminal "Default Window Settings" "Dracula"
-defaults write com.apple.terminal "Startup Window Settings" "Dracula"
+defaults write com.apple.terminal "Default Window Settings" "bullettrain"
+defaults write com.apple.terminal "Startup Window Settings" "bullettrain"
