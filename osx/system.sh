cb235c29429a49ca416325ff27dda7e8d741ad96
Dilip <ddprajapati@gmail.com>
2023-07-07 14:29:25 +0530

    Update system.sh

--- osx/system.sh
+++ osx/system.sh
@@ -2,24 +2,24 @@
 sudo nvram SystemAudioVolume=" "
 
 # Save to disk (not to iCloud) by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+defaults read NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Automatically quit printer app once the print jobs complete
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+defaults read com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 # Disable the crash reporter
-defaults write com.apple.CrashReporter DialogType -string "none"
+defaults read com.apple.CrashReporter DialogType -string "none"
 
 
 ## Track pad & Keyboard
 
 # Use scroll gesture with the Ctrl (^) modifier key to zoom
-defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+defaults read com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults read com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
 # Set a blazingly fast keyboard repeat rate
-#defaults write NSGlobalDomain KeyRepeat -int 1
-#defaults write NSGlobalDomain InitialKeyRepeat -int
+#defaults read NSGlobalDomain KeyRepeat -int 1
+#defaults read NSGlobalDomain InitialKeyRepeat -int
 
 # Stop iTunes from responding to the keyboard media keys
 #launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
@@ -27,46 +27,46 @@ defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 ## Finder
 
 # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-defaults write com.apple.finder QuitMenuItem -bool true
+defaults read com.apple.finder QuitMenuItem -bool true
 
 # Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
+defaults read com.apple.finder ShowPathbar -bool true
 
 # show status bar
-defaults write com.apple.finder ShowStatusBar -bool true
+defaults read com.apple.finder ShowStatusBar -bool true
 
 # Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults read NSGlobalDomain AppleShowAllExtensions -bool true
 
 # When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults read com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults read com.apple.finder _FXSortFoldersFirst -bool true
 
 # Avoid creating .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+defaults read com.apple.desktopservices DSDontreadNetworkStores -bool true
+defaults read com.apple.desktopservices DSDontreadUSBStores -bool true
 
 
 ## Mac App Store                                                               #
 
 # Enable the automatic update check
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+defaults read com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
 
 # Download newly available updates in background
-defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+defaults read com.apple.SoftwareUpdate AutomaticDownload -int 1
 
 # Install System data files & security updates
-defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+defaults read com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
 
 # Turn on app auto-update
-defaults write com.apple.commerce AutoUpdate -bool true
+defaults read com.apple.commerce AutoUpdate -bool true
 
 # Disallow the App Store to reboot machine on macOS updates
-defaults write com.apple.commerce AutoUpdateRestartRequired -bool false
+defaults read com.apple.commerce AutoUpdateRestartRequired -bool false
 
 ## Photos
 
 # Prevent Photos from opening automatically when devices are plugged in
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+defaults -currentHost read com.apple.ImageCapture disableHotPlug -bool true
