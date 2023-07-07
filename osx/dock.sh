924309417888fc999f48263e4564abb5e3f1c3d7
Dilip Prajapati <dilip.prajapati@heliossolutions.co>
2023-07-07 14:33:51 +0530

    revert

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Set Dark Theme to Dock & Fullscreen
-defaults read NSGlobalDomain AppleInterfaceStyle Dark;
+defaults write NSGlobalDomain AppleInterfaceStyle Dark;
 
 # Remove the auto-hiding Dock delay
-defaults read com.apple.Dock autohide-delay -float 0
+defaults write com.apple.Dock autohide-delay -float 0
 
 # Automatically hide and show the Dock
-defaults read com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide -bool true
 
 # Set the icon size of Dock items to 36 pixels
-defaults read com.apple.dock tilesize -int 38
+defaults write com.apple.dock tilesize -int 38
 
 # Don’t animate opening applications from the Dock
-defaults read com.apple.dock launchanim -bool false
+defaults write com.apple.dock launchanim -bool false
 
 # Show indicator lights for open applications in the Dock
-defaults read com.apple.dock show-process-indicators -bool true
+defaults write com.apple.dock show-process-indicators -bool true
 
 # Removed genie animation
-defaults read com.apple.dock mineffect suck;
+defaults write com.apple.dock mineffect suck;
 
 # Removes bouncing animation
-defaults read com.apple.dock no-bouncing -bool true
+defaults write com.apple.dock no-bouncing -bool true
 
 # Enable highlight hover effect for the grid view of a stack (Dock)
-defaults read com.apple.dock mouse-over-hilite-stack -bool true
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
 # Speed up Mission Control animations
-defaults read com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock expose-animation-duration -float 0.1
 
 # Disable the Launchpad gesture (pinch with thumb and three fingers)
-defaults read com.apple.dock showLaunchpadGestureEnabled -int 0
+defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 
 # Remove All Apps From The Dock In OS X
-defaults read com.apple.dock persistent-apps -array
+defaults write com.apple.dock persistent-apps -array
 
 killall Dock
--- osx/screen.sh
+++ osx/screen.sh
@@ -7,68 +7,68 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Disable opening and closing window animations
-defaults read NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
 # Increase window resize speed for Cocoa applications
-defaults read NSGlobalDomain NSWindowResizeTime -float 0.001
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
 # Save screenshots to the desktop
-defaults read com.apple.screencapture location -string "$HOME/Desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
 # Save screenshots in JPG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults read com.apple.screencapture type -string "jpg"
+defaults write com.apple.screencapture type -string "jpg"
 
 # Chime When Charging
-defaults read com.apple.PowerChime ChimeOnAllHardware -bool true && \
+defaults write com.apple.PowerChime ChimeOnAllHardware -bool true && \
 open /System/Library/CoreServices/PowerChime.app
 
 # Dark Theme to menubar
-sudo defaults read /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey -bool true
+sudo defaults write /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey -bool true
 
 # Appearance: Graphite
-defaults read -g AppleAquaColorVariant -int 6
+defaults write -g AppleAquaColorVariant -int 6
 
 # Turn off keyboard illumination when computer is not used for 5 minutes
-defaults read com.apple.BezelServices kDimTime -int 300
+defaults write com.apple.BezelServices kDimTime -int 300
 
 # Thanks Addy Osmani for the below items
 
 # Set highlight color to green
-defaults read NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
+defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
 
 # Menu bar: hide the Time Machine, Volume, User, and Bluetooth icons
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-	defaults read "${domain}" dontAutoLoad -array \
+	defaults write "${domain}" dontAutoLoad -array \
 		"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
 		"/System/Library/CoreServices/Menu Extras/Volume.menu" \
 		"/System/Library/CoreServices/Menu Extras/User.menu"
 done
-defaults read com.apple.systemuiserver menuExtras -array \
+defaults write com.apple.systemuiserver menuExtras -array \
 	"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
 	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
 	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
 	"/System/Library/CoreServices/Menu Extras/Clock.menu"
 
 # Remove the auto-hiding Dock delay
-defaults read com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-delay -float 0
 
 # Remove the animation when hiding/showing the Dock
-defaults read com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
 
 # Disable Dashboard
-defaults read com.apple.dashboard mcx-disabled -bool true
+defaults write com.apple.dashboard mcx-disabled -bool true
 
 # Automatically hide and show the Dock
-defaults read com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide -bool true
 
 # Fix mojave rendering issue
-defaults read -g CGFontRenderingFontSmoothingDisabled -bool FALSE
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool FALSE
 
 # Reset Launchpad
 find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
 
 # Make Dock more transparent
-defaults read com.apple.dock hide-mirror -bool true
+defaults write com.apple.dock hide-mirror -bool true
 
 # Disable Notification Center and remove the menu bar icon
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
@@ -76,9 +76,9 @@ launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.
 ## Hot corners
 
 # Top right screen corner → Desktop
-defaults read com.apple.dock wvous-tr-corner -int 4
-defaults read com.apple.dock wvous-tr-modifier -int 1048576
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 1048576
 
 # Bottom left screen corner → Start screen saver
-defaults read com.apple.dock wvous-bl-corner -int 3
-defaults read com.apple.dock wvous-bl-modifier -int 1048576
+defaults write com.apple.dock wvous-bl-corner -int 3
+defaults write com.apple.dock wvous-bl-modifier -int 1048576
