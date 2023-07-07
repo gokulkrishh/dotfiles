720818f1668698b75a2a9ca186faaf79c345ada2
Dilip <ddprajapati@gmail.com>
2023-07-07 14:27:59 +0530

    Update dock.sh

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Set Dark Theme to Dock & Fullscreen
-defaults write NSGlobalDomain AppleInterfaceStyle Dark;
+defaults read NSGlobalDomain AppleInterfaceStyle Dark;
 
 # Remove the auto-hiding Dock delay
-defaults write com.apple.Dock autohide-delay -float 0
+defaults read com.apple.Dock autohide-delay -float 0
 
 # Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
+defaults read com.apple.dock autohide -bool true
 
 # Set the icon size of Dock items to 36 pixels
-defaults write com.apple.dock tilesize -int 38
+defaults read com.apple.dock tilesize -int 38
 
 # Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool false
+defaults read com.apple.dock launchanim -bool false
 
 # Show indicator lights for open applications in the Dock
-defaults write com.apple.dock show-process-indicators -bool true
+defaults read com.apple.dock show-process-indicators -bool true
 
 # Removed genie animation
-defaults write com.apple.dock mineffect suck;
+defaults read com.apple.dock mineffect suck;
 
 # Removes bouncing animation
-defaults write com.apple.dock no-bouncing -bool true
+defaults read com.apple.dock no-bouncing -bool true
 
 # Enable highlight hover effect for the grid view of a stack (Dock)
-defaults write com.apple.dock mouse-over-hilite-stack -bool true
+defaults read com.apple.dock mouse-over-hilite-stack -bool true
 
 # Speed up Mission Control animations
-defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults read com.apple.dock expose-animation-duration -float 0.1
 
 # Disable the Launchpad gesture (pinch with thumb and three fingers)
-defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+defaults read com.apple.dock showLaunchpadGestureEnabled -int 0
 
 # Remove All Apps From The Dock In OS X
-defaults write com.apple.dock persistent-apps -array
+defaults read com.apple.dock persistent-apps -array
 
 killall Dock
